43058e4c83256f6e8d3490109c0d4824a84bb182
Juan-Vasquez <vasquez-juan@hotmail.com>
2022-01-19 19:46:33 -0600

    creacion del test para obtener todos los recursos

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -6,9 +6,15 @@ use App\Article;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleResource;
+use App\Http\Resources\ArticleCollection;
 
 class ArticleController extends Controller
 {
+	public function index()
+	{
+		return ArticleCollection::make(Article::all());
+	}
+
    	public function show(Article $article)
    	{
    		return ArticleResource::make($article);
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('articles/{article}', 'Api\ArticleController@show')->name('api.v1.show');
+Route::get('articles', 'Api\ArticleController@index')->name('api.v1.articles.index');
+Route::get('articles/{article}', 'Api\ArticleController@show')->name('api.v1.articles.show');
